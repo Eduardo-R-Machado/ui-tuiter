3ed85e57a0e2c723c6e4be656f4a97da2e4c0fec
Eduardo-r-machado <eduardormachado20@gmail.com>
2023-03-23 00:21:08 -0300

    tempo de aula 1:14 / importando CSS

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -17,7 +17,7 @@ export default function Tweet(props: tweetProps) {
     <div>
       <h1><b>{props.user}</b></h1>
       <h3>{props.children}</h3>
-      <h4>Likes {likes ?? 0}</h4>
+      <h4>Likes {likes}</h4>
       <button onClick={handleLike}>Dar Like</button>
     </div>
   );
